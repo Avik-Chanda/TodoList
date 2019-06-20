fc753ce4e85c35ee73fb5925817310b6ebe9ccb2
avikchanda <a.chanda1993@gmail.com>
2019-06-21 00:08:08 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FIZZBUZZ';
+export class AppComponent implements OnInit{
+  
+  title = 'Ag Grid Demo';
+
+  list  =  [];
+
+  name :string = ''
+
+
+  ngOnInit() {
+   }
+
+
+   add(name : string)
+   {
+      this.list.unshift(name);
+
+      console.log(this.list)
+
+      this.name =''  
+   }
+
+   remove(i: number)
+   {
+      this.list.splice(i,1)
+   }
+
+
 }
+
+
+
+  
+
+  
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+import {AgGridModule } from 'ag-grid-angular'
+import { FormsModule } from '@angular/forms'
+ import { AppComponent } from './app.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgGridModule.withComponents([AppComponent]),
+    RouterModule.forRoot([
+      {
+        path : '',
+        component : AppComponent
+      }
+    ])
   ],
   providers: [],
+  entryComponents :[],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
